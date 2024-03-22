adbdc6f58a3e46b08bc92e22f43cf5b5d23835d7
akgunkaya <akgunkaya12@gmail.com>
2024-03-22 22:32:16 +0000

    added markup and styling for navbar

--- wp-content/themes/eval/header.php
+++ wp-content/themes/eval/header.php
@@ -1,28 +1,48 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width" />
+  <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<div id="branding">
-<div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-<?php
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
-echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
-?>
-</div>
-<div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
-</div>
-<nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
-<div id="search"><?php get_search_form(); ?></div>
-</nav>
-</header>
-<div id="container">
-<main id="content" role="main">
+  <?php wp_body_open(); ?>
+  <div id="wrapper" class="hfeed">
+    <header id="header" role="banner">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="<?php echo home_url(); ?>">
+            <span class="material-icons">
+              home
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <?php
+            $menu = wp_get_nav_menu_object('main-menu');
+
+            if ($menu) {
+              $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+              if (!empty($menu_items)) {
+                echo '<ul class="navbar-nav">';
+                foreach ($menu_items as $menu_item) {
+                  echo '<li class="nav-item"><a class="nav-link" href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
+                }
+                echo '</ul>';
+              }
+            }
+            ?>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <div id="container">
+      <main id="content" role="main">
