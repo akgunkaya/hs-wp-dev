727560900071c8f7b12843f5e98837da436f31f5
akgunkaya <akgunkaya12@gmail.com>
2024-03-24 15:58:20 +0000

    added ipapi functionality

--- wp-content/themes/eval/header.php
+++ wp-content/themes/eval/header.php
@@ -7,19 +7,51 @@
   <?php wp_head(); ?>
 </head>
 
+<?php
+
+function getCityFromIPAPI()
+{
+  $curl = curl_init();
+
+  curl_setopt($curl, CURLOPT_URL, "https://ipapi.co/city/");
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+
+  $response = curl_exec($curl);
+  $contentType = curl_getinfo($curl, CURLINFO_CONTENT_TYPE);
+
+  curl_close($curl);
+
+  if (strpos($contentType, 'text/plain') !== false && is_string($response) && !empty($response)) {
+    return 'in ' . $response;
+  } else {
+    return "everywhere";
+  }
+}
+?>
+
+
+
+
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
   <div id="wrapper" class="hfeed">
     <header id="header" role="banner">
       <nav class="navbar navbar-expand-lg navbar-light ">
         <div class="container-fluid">
-          <a class="navbar-brand" href="<?php echo home_url(); ?>">
-            <span class="material-symbols-outlined">
-              home
+          <div class="d-flex align-items-center">
+            <a class="navbar-brand" href="<?php echo home_url(); ?>">
+              <span class="material-symbols-outlined">
+                home
+              </span>
+            </a>
+            <span class="ipapi-target">
+              Our Gutters available
+              <span><?php echo getCityFromIPAPI(); ?></span>
             </span>
-          </a>
+          </div>
 
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button class=" navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
 
